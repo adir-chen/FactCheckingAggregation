69414109d8801ba214b19875ebcafcd80d356b7d
Elad Birnfeld <elad4884@gmail.com>
2018-12-20 18:37:21 +0200

    deleted intensity

--- claims/urls.py
+++ claims/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+    path('', views.view_home, name='home_page'),
     path('add_claim', views.add_claim, name='add_claim'),
-    path('claim', views.view_claim, name='view_claim'),
+    # view_claim should have: data="claim_id:#"
+    path('claim/<int:id>', views.view_claim, name='view_claim'),
 ]
