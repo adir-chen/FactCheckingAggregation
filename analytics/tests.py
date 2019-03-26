4df26ac6a179d6d1c8ef39449235ab61e2be56a6
adirc <adirc@post.bgu.ac.il>
2019-03-26 22:59:10 +0200

    Changes in analytics

--- analytics/tests.py
+++ analytics/tests.py
@@ -1,24 +1,30 @@
 from django.contrib.auth.models import User
 from django.http import HttpRequest, Http404
 from django.test import TestCase
-from analytics.views import view_analytics_default
+from analytics.views import view_analytics
 
 
 class Analytics(TestCase):
     def setUp(self):
+        self.admin = User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')
         self.user_1 = User(username="User1", email='user1@gmail.com')
         self.user_1.save()
         self.get_request = HttpRequest()
         self.get_request.method = 'GET'
+        self.post_request = HttpRequest()
+        self.post_request.method = 'POST'
 
     def tearDown(self):
         pass
 
     def test_view_analytics_valid_user(self):
-        admin = User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')
-        self.get_request.user = admin
-        self.assertTrue(view_analytics_default(self.get_request).status_code == 200)
+        self.get_request.user = self.admin
+        self.assertTrue(view_analytics(self.get_request).status_code == 200)
 
     def test_view_analytics_invalid_user(self):
         self.get_request.user = self.user_1
-        self.assertRaises(Http404, view_analytics_default, self.get_request)
+        self.assertRaises(Http404, view_analytics, self.get_request)
+
+    def test_view_analytics_invalid_request(self):
+        self.post_request.user = self.admin
+        self.assertRaises(Http404, view_analytics, self.post_request)
--- analytics/urls.py
+++ analytics/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "analytics"
 urlpatterns = [
     path('', views.view_analytics, name='view_analytics'),
-    path('view_analytics_customized', views.view_analytics_customized, name='view_analytics_customized'),
+    path('view_customized_analytics', views.view_customized_analytics, name='view_customized_analytics'),
     path('view_top_n_claims', views.view_top_n_claims, name='view_top_n_claims'),
 ]
