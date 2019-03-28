12a7db05d1a83cd964cfda000a078302b18ef6e2
Elad Birnfeld <elad4884@gmail.com>
2019-03-28 14:31:16 +0200

    little fixes

--- search/views.py
+++ search/views.py
@@ -11,7 +11,8 @@ def search(request):
     if request.method != "GET":
         raise Http404("Permission denied")
     keywords = request.GET.get('search_keywords')
-    claims = Claim.objects.filter(Q(claim__icontains=keywords) | Q(tags__icontains=keywords)).order_by('-id')
+    claims = Claim.objects.filter(Q(claim__icontains=keywords) | Q(tags__icontains=keywords) |
+                                  Q(user__username__icontains=keywords)).order_by('-id')
     search_result = list(get_users_images_for_claims(claims).items())
     page = request.GET.get('page')
     paginator = Paginator(search_result, 24)
