15ee369dc1ec82cda0a42a92f95f704a4b068322
Elad Birnfeld <elad4884@gmail.com>
2019-03-28 11:41:52 +0200

    changed username to user_id in profile page

--- users/views.py
+++ users/views.py
@@ -325,13 +325,13 @@ def update_reputation_for_user(user_id, earn_points, num_of_points):
 
 
 # This function returns a HTML for a user's profile
-def user_page(request, username):
+def user_page(request, user_id):
     from django.contrib.sessions.models import Session
     if request.method != 'GET':
         raise Http404("Permission denied")
-    user = get_user_by_username(username)
+    user = User.objects.filter(id=user_id).first()
     if user is None:
-        raise Http404('User ' + username + ' does not exist')
+        raise Http404('User with id ' + user_id + ' does not exist')
     decoded_sessions = [s.get_decoded() for s in Session.objects.all()]
     logged_in_users = [int(s.get('_auth_user_id')) for s in decoded_sessions]
     logged_in = user.id in logged_in_users
@@ -363,9 +363,9 @@ def user_page(request, username):
         'user_img': user_img,
         'user_rep': user_rep,
         'scrapers_ids': get_all_scrapers_ids_arr(),
-        'scraper_url': get_scraper_url(username),
-        'true_labels': get_true_labels(username),
-        'false_labels': get_false_labels(username)
+        'scraper_url': get_scraper_url(user.username),
+        'true_labels': get_true_labels(user.username),
+        'false_labels': get_false_labels(user.username)
     })
 
 
