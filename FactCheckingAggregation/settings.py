bf972d41b44e574a4cdea062313da4b8fb7f2c16
adirc <adirc@post.bgu.ac.il>
2019-04-01 09:25:47 +0300

    Sending email

--- FactCheckingAggregation/settings.py
+++ FactCheckingAggregation/settings.py
@@ -86,7 +86,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'wtfactnews@gmail.com'
 EMAIL_HOST_PASSWORD = 'amc8dGig'
-EMAIL_PORT = 465
+EMAIL_PORT = 567
 EMAIL_USE_TLS = True
 
 TEMPLATES = [
