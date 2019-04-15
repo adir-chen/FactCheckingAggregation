dad2006ba408e04e64d8d27ed2b0cf67acbbf10b
adirc <adirc@post.bgu.ac.il>
2019-04-15 15:19:41 +0300

    Updated wsgi

--- FactCheckingAggregation/wsgi.py
+++ FactCheckingAggregation/wsgi.py
@@ -11,9 +11,6 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 path = '/home/birnfeld/Documents/FactCheckingAggregation'
-if path not in sys.path:
-    sys.path.append(path)
+sys.path.append(path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FactCheckingAggregation.settings')
-# os.environ["DJANGO_SETTINGS_MODULE"] = "{{ FactCheckingAggregation }}.settings"
-
 application = get_wsgi_application()
