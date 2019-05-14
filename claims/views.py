01a8e2d8d8e7d734f92c9d603075ebb2d2a93052
adirc <adirc@post.bgu.ac.il>
2019-05-14 23:09:17 +0300

    Fixed notifications for admin

--- claims/views.py
+++ claims/views.py
@@ -286,7 +286,7 @@ def report_spam(request):
     save_log_message(request.user.id, request.user.username,
                      'Reporting a claim with id ' + str(request.POST.get('claim_id')) + ' as spam', True)
     superusers = User.objects.filter(is_superuser=True).exclude(id=request.user.id)
-    notify.send(request.user, recipient=superusers, verb='Report claim https://wtfact.ise.bgu.ac.il/claim/' + str(request.POST.get('claim_id')) + ' as spam', target=Claim.objects.filter(id=request.POST.get('claim_id')).first())
+    notify.send(request.user, recipient=superusers, verb='reported claim https://wtfact.ise.bgu.ac.il/claim/' + str(request.POST.get('claim_id')) + ' as spam', target=Claim.objects.filter(id=request.POST.get('claim_id')).first())
     return view_claim(return_get_request_to_user(request.user), request.POST.get('claim_id'))
 
 
