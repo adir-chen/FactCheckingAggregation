a51669c06a24f21b6369d02ba385cfab028b80e4
adirc <adirc@post.bgu.ac.il>
2019-05-15 21:27:15 +0300

    Use tweet as comment ref

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,9 @@
+import json
+import tweepy
 from django.contrib.auth.models import User
 from django.utils import timezone
+from tweepy import OAuthHandler
+from FactCheckingAggregation import settings
 from claims.models import Claim
 from django.db import models
 
@@ -12,4 +16,19 @@ class Tweet(models.Model):
     def __str__(self):
         return str(self.claim.id) + ' - ' + self.tweet_link
 
-
+    def get_tweet_data(self):
+        auth = OAuthHandler(settings.twitter_consumer_key, settings.twitter_consumer_secret)
+        auth.set_access_token(settings.twitter_access_token, settings.twitter_access_secret)
+        api = tweepy.API(auth)
+        tweet_data = {'tweet_title': '',
+                      'tweet_text': '',
+                      'tweet_date': ''}
+        try:
+            tweet = api.get_status(self.tweet_link.split('status/')[1])
+            if tweet:
+                tweet_data['tweet_title'] = tweet.user.name + "'s" + ' tweet'
+                tweet_data['tweet_text'] = tweet.text.split('https')[0]
+                tweet_data['tweet_date'] = str(tweet.created_at.date())
+        except:
+            pass
+        return json.dumps(tweet_data)
